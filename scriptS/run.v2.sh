5b1b974e88add249aea35ab59c26a699ecd1053f
Your Name <you@example.com>
2020-07-14 01:49:28 -0700

    20200714_0149AM_

--- scriptS/run.v2.sh
+++ scriptS/run.v2.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    aa1=`basename ${PWD}`
-    for bb1 in x2 test2  ; do
-        if [ "${bb1}" = "${aa1}" ]; then
-            if [ -f default.json ]; then
-                echo "nice -n 19 ./caddy  run -config default.json"
-                nice       -n 19 ./caddy  run -config default.json 
-            else
-                echo "nice -n 19 ./caddy  run"
-                nice       -n 19 ./caddy  run 
-            fi
-            fi
-        done
-else
-                echo "nice -n 19 ./caddy  run -config $1"
-                nice       -n 19 ./caddy  run -config $1 
+# git pull ; nice -n 19 ./caddy.4    run -config default.json.42018.100.json
+
+prog=./caddy 
+for aa1 in ./caddy.1 ./caddy.2 ./caddy.3 ./caddy.4 
+do
+   if [ -f ${aa1} ] ; then
+       prog=${aa1}
+   fi
+done
+
+for aa1 in default.json default.json.42018.100.json
+do
+   if [ -f ${aa1} ] ; then
+       ff1=${aa1}
+   fi
+done
+if [ -n "$1" ]; then
+    ff1=$1
 fi
+
+echo "nice -n 19 ${prog}  run -config ${ff1}"
+nice       -n 19 ${prog}  run -config ${ff1} 
+echo "nice -n 19 ${prog}  run -config ${ff1}"
